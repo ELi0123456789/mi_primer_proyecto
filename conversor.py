4b6d4953af7e7652f276281de2bfacd8d4d14a11
Eli0123456789 <tu@email.com>
2026-04-07 18:52:23 -0600

    arreglo de key

--- conversor.py
+++ conversor.py
@@ -1,32 +1,36 @@
 import requests
 
-def convertir_momneda():
-        print("Conversor de monedas")
-        api_key = "602d33405781a539b52479e0" # Esta es una key de prueba
-        url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD"
-        
+def convertir_moneda():
+    print("--- Conversor de Monedas Express ---")
+    
+    # Configuración de la API (Uso de una clave gratuita de ejemplo)
+    api_key = "b0b85b07058c4c6a20bb0570" # Esta es una key de prueba
+    url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD"
+
     try:
+        # Obtenemos los datos
         respuesta = requests.get(url)
         datos = respuesta.json()
 
         if respuesta.status_code != 200:
-            print("error al conectar a la api")    
+            print("Error al conectar con la API.")
             return
-        
-        monto = float(input("ingresa la cantidad en USD: "))
-        moneda_destino = input("ingresa la moneda destino: ")
 
+        # Pedir datos al usuario
+        monto = float(input("Ingresa la cantidad en USD: "))
+        moneda_destino = input("Ingresa la moneda de destino (ej: MXN, EUR, ARS): ").upper()
+
+        # Lógica de conversión
         if moneda_destino in datos['conversion_rates']:
-               tasa = datos['conversion_rates'][moneda_destino]
-               resultado = monto * tasa
-               print("f\n{monto} USD equivalen a {resultado:.2f}{moneda_destino}")
-               print("tasa de cambio actual: {tasa}")
+            tasa = datos['conversion_rates'][moneda_destino]
+            resultado = monto * tasa
+            print(f"\n{monto} USD equivalen a {resultado:.2f} {moneda_destino}")
+            print(f"Tasa de cambio actual: {tasa}")
         else:
-               print("esa moneda no esta disponible")
+            print("Esa moneda no está disponible.")
+
     except Exception as e:
-       print(f"ocurrio un problema {e}")
+        print(f"Ocurrió un error: {e}")
 
-if  __name__ == "__main__":
-       convertir_momneda()
-
-
+if __name__ == "__main__":
+    convertir_moneda()
